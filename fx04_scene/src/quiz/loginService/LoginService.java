fe986469cdeecbadf1d4c90498839679d3df5568
jungyooseok <wjddbtjr8888@naver.com>
2021-09-05 21:19:13 +0900

    fx04

---
+++ fx04_scene/src/quiz/loginService/LoginService.java
@@ -0,0 +1,9 @@
+package quiz.loginService;
+
+import javafx.scene.Parent;
+
+public interface LoginService {
+	public void LoginProc(Parent root);
+	public void CancelProc(Parent root);
+	public void OpenMembershipForm(Parent root);
+}
